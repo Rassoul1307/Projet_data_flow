609af87ce90d8af95ea23fb8962143372c84614e
Ahmed Fall <127935304+Rassoul1307@users.noreply.github.com>
2025-07-30 13:46:13 +0000

    generation de donnees meteo via script python

---
+++ collecte/generation/rassoul_data.py
@@ -0,0 +1,46 @@
+from faker import Faker
+import random
+from datetime import datetime, timedelta
+import json
+
+
+fake = Faker()
+
+def generate_weather_data(start_date, num_days):
+    weather_data = []
+    current_date = start_date
+
+    with open("./data/generation/rassoul_data_meteo.json", "w", encoding="utf-8") as file:
+
+        for _ in range(num_days):
+            data = {
+                    "ville": "Dakar",
+                    "longitude": 14.693425,
+                    "latitude": -17.447938,
+                    "Date": current_date.strftime("%d/%m/%Y %H:%M:%S"),
+                    "température": f"{random.randint(10, 30)} °C",
+                    "humidité": f"{random.randint(40, 80)} %",
+                    "couverture_nuages": f"{random.randint(0,80)} %",
+                    "intensite_pluies": f"{random.randint(0,70)} mm/h",
+                    "vitesse_vent": f"{random.randint(0,20)} km/h",
+                }
+            
+            # Écrire chaque objet JSON sur une ligne séparée
+            file.write(json.dumps(data, ensure_ascii=False) + "\n")
+
+            weather_data.append(data)
+            current_date -= timedelta(minutes=15)
+
+       
+
+        #print(weather_data)
+
+    return weather_data
+
+start_date = datetime.now()
+
+
+generate_weather_data(start_date, 1000)
+#save_to_json(generate_weather_data(start_date, 100), "./data/weather_data.json")
+
+
